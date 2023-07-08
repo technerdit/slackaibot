35783ea42f7349f7ed6893b3d7dccf7aff1034cc
Tech Nerd <eric@thetechnerdit.com>
2023-07-07 17:15:32 -0700

    Updated to use environmental variables for secrets

--- openaislack/slackauth.py
+++
@@ -1,9 +0,0 @@
-from slack_sdk import WebClient
-
-class SlackAuth(object):
-    def __init__(self, **kwargs):
-        self.client_id = kwargs['client_id']
-        self.client_secret = kwargs['client_secret']
-        self.code = kwargs['code_param']
-
-    def gettoken(self):
